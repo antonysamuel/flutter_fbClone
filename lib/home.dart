7c7cf7c26500c854a9f1e74148ed5878579c50f4
Samuel <antonysamuel22@outlook.com>
2021-04-11 17:01:22 +0530

    created appbar and added dummy functions

--- lib/home.dart
+++ lib/home.dart
@@ -1,11 +1,31 @@
+import 'package:facebook_clone/widgets/appbarBtn.dart';
 import 'package:flutter/material.dart';
-
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        title: Text(
+          "facebook",
+          style: TextStyle(
+              color: Colors.blue, fontSize: 28, fontWeight: FontWeight.bold),
+        ),
+        actions: [
+          AppBarButton(
+              buttonIcon: Icons.search,
+              buttonAction: () {
+                print("search");
+              }),
+          AppBarButton(
+              buttonIcon: Icons.chat,
+              buttonAction: () {
+                print("messenger");
+              }),
+        ],
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:facebook_clone/home.dart';
 import 'package:flutter/material.dart';
 
-void main(){
-
+void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     home: Home(),
   ));
 }
